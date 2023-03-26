9faac6313d1e93b4e331fb1149939d9f1de0f7b6
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2023-03-26 16:11:14 +0200

    add documentation to args

--- src/chat.rs
+++ src/chat.rs
@@ -13,27 +13,50 @@ use serde::{Deserialize, Serialize};
 /// ]);
 /// ```
 /// 
+/// To use streaming, use [crate::Client::create_chat_stream].
+/// 
 #[derive(Serialize, Debug, Clone)]
 pub struct ChatArguments {
+    /// ID of the model to use. See the model [endpoint compatibility table](https://platform.openai.com/docs/models/model-endpoint-compatibility) for details on which models work with the Chat API.
     pub model: String,
+    /// The [Message]s to generate chat completions for
     pub messages: Vec<Message>,
+    /// What sampling temperature to use, between 0 and 2. Higher values like 0.8 will make the output more random, while lower values like 0.2 will make it more focused and deterministic.
+    /// 
+    /// We generally recommend altering this or top_p but not both.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub temperature: Option<f32>,
+    /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+    /// 
+    /// We generally recommend altering this or temperature but not both.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub top_p: Option<f32>,
+    /// How many chat completion choices to generate for each input message.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub n: Option<u32>,
     #[serde(skip_serializing_if = "Option::is_none")]
-    pub stream: Option<bool>,
+    pub(crate) stream: Option<bool>,
+    /// Up to 4 sequences where the API will stop generating further tokens.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub stop: Option<String>,
+    /// The maximum number of [tokens](https://platform.openai.com/tokenizer) to generate in the chat completion.
+    ///
+    /// The total length of input tokens and generated tokens is limited by the model's context length.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub max_tokens: Option<u32>,
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far,
+    /// increasing the model's likelihood to talk about new topics.
+    ///
+    /// [See more information about frequency and presence penalties.](https://platform.openai.com/docs/api-reference/parameter-details)
     #[serde(skip_serializing_if = "Option::is_none")]
     pub presence_penalty: Option<f32>,
+    /// Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far,
+    /// decreasing the model's likelihood to repeat the same line verbatim.
     #[serde(skip_serializing_if = "Option::is_none")]
     pub frequency_penalty: Option<f32>,
     // logit_bias
+    /// A unique identifier representing your end-user, which can help OpenAI to monitor and detect abuse.
+    /// [Learn more](https://platform.openai.com/docs/guides/safety-best-practices/end-user-ids).
     #[serde(skip_serializing_if = "Option::is_none")]
     pub user: Option<String>,
 }
