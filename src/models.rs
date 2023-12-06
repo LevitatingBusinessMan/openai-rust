c926063eed98f3d228fbb6dcd85bea944baada79
LevitatingBusinessMan (Rein Fernhout) <me@levitati.ng>
2023-12-06 03:08:23 +0100

    fixed models

--- src/models.rs
+++ src/models.rs
@@ -5,26 +5,29 @@ pub(crate) struct ListModelsResponse {
     pub data: Vec<Model>,
 }
 
-/// A model with permissions
+/// Describes an OpenAI model offering that can be used with the API.
 #[derive(Deserialize, Debug)]
 pub struct Model {
+    /// The model identifier, which can be referenced in the API endpoints.
     pub id: String,
+    /// The organization that owns the model.
     pub owned_by: String,
-    pub permission: Vec<ModelPermission>,
+    /// The Unix timestamp (in seconds) when the model was created.
+    pub created: u64,
 }
 
-/// Permissions of a model
-#[derive(Deserialize, Debug)]
-pub struct ModelPermission {
-    pub id: String,
-    pub created: u32,
-    pub allow_create_engine: bool,
-    pub allow_sampling: bool,
-    pub allow_logprobs: bool,
-    pub allow_search_indices: bool,
-    pub allow_view: bool,
-    pub allow_fine_tuning: bool,
-    pub organization: String,
-    pub group: Option<String>,
-    pub is_blocking: bool,
-}
+// /// Permissions of a model
+// #[derive(Deserialize, Debug)]
+// pub struct ModelPermission {
+//     pub id: String,
+//     pub created: u32,
+//     pub allow_create_engine: bool,
+//     pub allow_sampling: bool,
+//     pub allow_logprobs: bool,
+//     pub allow_search_indices: bool,
+//     pub allow_view: bool,
+//     pub allow_fine_tuning: bool,
+//     pub organization: String,
+//     pub group: Option<String>,
+//     pub is_blocking: bool,
+// }
